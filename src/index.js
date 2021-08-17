5e377dba8ffaa62e33efbe46c7563ba9f480eec4
KokiYamada13 <44269375+KokiYamada13@users.noreply.github.com>
2021-08-17 17:33:41 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// import { StrictMode } from "react";
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { App } from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   rootElement
+// );
